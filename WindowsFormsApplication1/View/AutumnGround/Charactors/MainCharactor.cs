7e4a322b02609568fd5626b21e74f4532b4cdf68
kenta_osawa <twinkle.pp.753-k@i.softbank.jp>
2015-11-20 17:23:02 +0900

    主人公を描画

--- WindowsFormsApplication1/View/AutumnGround/Charactors/MainCharactor.cs
+++ WindowsFormsApplication1/View/AutumnGround/Charactors/MainCharactor.cs
@@ -6,20 +6,36 @@ using shuntamu.Util;
 
 namespace shuntamu.View.AutumnGround.Charactors
 {
-    class MainCharactor : MotionObject
+    internal enum MainCharactorStatus
+    {
+        Idle,
+        Run,
+        Jump,
+        Fall
+    }
+
+    internal class MainCharactor : MotionObject
     {
         public MainCharactor()
             : base(SaveObject.RestartPoint, new Size(10, 20))
         {
+            MainCharactorStatus = MainCharactorStatus.Idle;
             Direction = Direction.Right;
             HitYEvent += obj =>
             {
                 if (obj.IsSolid)
                 {
                     _vy = 0;
-                    if (Input.Instance.LeftShift)
+                    if (obj.Top.Y >= Bottom.Y)
                     {
-                        if (obj.Top.Y >= Bottom.Y)
+
+                        if (Vx != 0)
+                            MainCharactorStatus = MainCharactorStatus.Run;
+                        else
+                        {
+                            MainCharactorStatus = MainCharactorStatus.Idle;
+                        }
+                        if (Input.Instance.LeftShift)
                         {
                             _vy = -3;
                             jumpflame = 1;
@@ -40,19 +56,41 @@ namespace shuntamu.View.AutumnGround.Charactors
                 obj.OnBeHitEvent();
                 if (obj is SaveObject)
                 {
-                    ((SaveObject)obj).Save();
+                    ((SaveObject) obj).Save();
                 }
                 if (obj is IKiller)
                 {
-                    ((IKiller)obj).Kill();
+                    ((IKiller) obj).Kill();
                     // DX.DrawString(400, 200, "Game Over", DX.GetColor(200, 200, 200));
                 }
             };
 
         }
 
+        private long time = 0;
         private float _vx = 0;
         private float _vy = 0;
+        public MainCharactorStatus MainCharactorStatus { get; set; }
+
+        public double Flame20
+        {
+            get { return (time%20)/20.0; }
+        }
+
+        public double Flame50
+        {
+            get { return (time%50)/50.0; }
+        }
+
+        public double Flame100
+        {
+            get { return (time%100)/100.0; }
+        }
+
+        public double Flame1000
+        {
+            get { return (time % 1000) / 1000.0; }
+        }
 
         private int Vx
         {
@@ -68,9 +106,9 @@ namespace shuntamu.View.AutumnGround.Charactors
                 }
                 else
                 {
-                    _vx += +ax / m;
+                    _vx += +ax/m;
                 }
-                return (int)_vx;
+                return (int) _vx;
             }
         }
 
@@ -88,9 +126,9 @@ namespace shuntamu.View.AutumnGround.Charactors
                 }
                 else
                 {
-                    _vy += +ay / m;
+                    _vy += +ay/m;
                 }
-                return (int)_vy;
+                return (int) _vy;
             }
         }
 
@@ -100,8 +138,10 @@ namespace shuntamu.View.AutumnGround.Charactors
         private float m = 10f;
         private int _bulletIntervalCount = BULLETINTERVAL;
         private const int BULLETINTERVAL = 20;
+
         public override void Update(MapBase map)
         {
+            time++;
             if (_bulletIntervalCount > 0)
             {
                 _bulletIntervalCount--;
@@ -165,17 +205,164 @@ namespace shuntamu.View.AutumnGround.Charactors
                 ax = 0;
                 _vx = 0;
             }
+            if (Vy < 0)
+            {
+                MainCharactorStatus = MainCharactorStatus.Jump;
+            }
+            else if (Vy > 5)
+            {
+                MainCharactorStatus = MainCharactorStatus.Fall;
+            }
             base.Update(map);
             Distance = new Point(Vx, Vy);
+
         }
 
         public Direction Direction { get; set; }
 
+        private int playerIdleHandleR1 = DX.LoadGraph(@"../../IWBT素材/スプライト/sprPlayerIdleR1.png");
+        private int playerIdleHandleR2 = DX.LoadGraph(@"../../IWBT素材/スプライト/sprPlayerIdleR2.png");
+        private int playerIdleHandleR3 = DX.LoadGraph(@"../../IWBT素材/スプライト/sprPlayerIdleR3.png");
+        private int playerIdleHandleR4 = DX.LoadGraph(@"../../IWBT素材/スプライト/sprPlayerIdleR4.png");
+        private int playerIdleHandleL1 = DX.LoadGraph(@"../../IWBT素材/スプライト/sprPlayerIdleL1.png");
+        private int playerIdleHandleL2 = DX.LoadGraph(@"../../IWBT素材/スプライト/sprPlayerIdleL2.png");
+        private int playerIdleHandleL3 = DX.LoadGraph(@"../../IWBT素材/スプライト/sprPlayerIdleL3.png");
+        private int playerIdleHandleL4 = DX.LoadGraph(@"../../IWBT素材/スプライト/sprPlayerIdleL4.png");
+        private int playerRunHandleR1 = DX.LoadGraph(@"../../IWBT素材/スプライト/sprPlayerRunningR1.png");
+        private int playerRunHandleR2 = DX.LoadGraph(@"../../IWBT素材/スプライト/sprPlayerRunningR2.png");
+        private int playerRunHandleR3 = DX.LoadGraph(@"../../IWBT素材/スプライト/sprPlayerRunningR3.png");
+        private int playerRunHandleR4 = DX.LoadGraph(@"../../IWBT素材/スプライト/sprPlayerRunningR4.png");
+        private int playerRunHandleL1 = DX.LoadGraph(@"../../IWBT素材/スプライト/sprPlayerRunningL1.png");
+        private int playerRunHandleL2 = DX.LoadGraph(@"../../IWBT素材/スプライト/sprPlayerRunningL2.png");
+        private int playerRunHandleL3 = DX.LoadGraph(@"../../IWBT素材/スプライト/sprPlayerRunningL3.png");
+        private int playerRunHandleL4 = DX.LoadGraph(@"../../IWBT素材/スプライト/sprPlayerRunningL4.png");
+        private int playerJumpHandleR = DX.LoadGraph(@"../../IWBT素材/スプライト/sprPlayerJumpR.png");
+        private int playerJumpHandleL = DX.LoadGraph(@"../../IWBT素材/スプライト/sprPlayerJumpL.png");
+        private int playerFallHandleR = DX.LoadGraph(@"../../IWBT素材/スプライト/sprPlayerFallR.png");
+        private int playerFallHandleL = DX.LoadGraph(@"../../IWBT素材/スプライト/sprPlayerFallL.png");
 
         public override void Draw(Point top, Size size)
         {
-            // DX.DrawCircle(top.X, top.Y, 10, DX.GetColor(200, 200, 200));
-            base.Draw(top, size);
+            double flame = Flame20;
+            switch (MainCharactorStatus)
+            {
+                case MainCharactorStatus.Idle:
+                    if (Direction == Direction.Right)
+                    {
+                        if (flame <= 1/4.0)
+                        {
+                            DX.DrawGraph(top.X - 11, top.Y - 12, playerIdleHandleR1, DX.TRUE);
+                            return;
+                        }
+                        if (flame > 1/4.0 && flame <= 1/2.0)
+                        {
+                            DX.DrawGraph(top.X - 11, top.Y - 12, playerIdleHandleR2, DX.TRUE);
+                            return;
+                        }
+                        if (flame > 1/2.0 && flame <= 3/4.0)
+                        {
+                            DX.DrawGraph(top.X - 11, top.Y - 12, playerIdleHandleR3, DX.TRUE);
+                            return;
+                        }
+                        if (flame > 3/4.0)
+                        {
+                            DX.DrawGraph(top.X - 11, top.Y - 12, playerIdleHandleR4, DX.TRUE);
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        if (flame <= 1/4.0)
+                        {
+                            DX.DrawGraph(top.X - 11, top.Y - 12, playerIdleHandleL1, DX.TRUE);
+                            return;
+                        }
+                        if (flame > 1/4.0 && flame <= 1/2.0)
+                        {
+                            DX.DrawGraph(top.X - 11, top.Y - 12, playerIdleHandleL2, DX.TRUE);
+                            return;
+                        }
+                        if (flame > 1/2.0 && flame <= 3/4.0)
+                        {
+                            DX.DrawGraph(top.X - 11, top.Y - 12, playerIdleHandleL3, DX.TRUE);
+                            return;
+                        }
+                        if (flame > 3/4.0)
+                        {
+                            DX.DrawGraph(top.X - 11, top.Y - 12, playerIdleHandleL4, DX.TRUE);
+                            return;
+                        }
+                    }
+                    break;
+                case MainCharactorStatus.Run:
+                    if (Direction == Direction.Right)
+                    {
+                        if (flame <= 1 / 4.0)
+                        {
+                            DX.DrawGraph(top.X - 11, top.Y - 12, playerRunHandleR1, DX.TRUE);
+                            return;
+                        }
+                        if (flame > 1 / 4.0 && flame <= 1 / 2.0)
+                        {
+                            DX.DrawGraph(top.X - 11, top.Y - 12, playerRunHandleR2, DX.TRUE);
+                            return;
+                        }
+                        if (flame > 1 / 2.0 && flame <= 3 / 4.0)
+                        {
+                            DX.DrawGraph(top.X - 11, top.Y - 12, playerRunHandleR3, DX.TRUE);
+                            return;
+                        }
+                        if (flame > 3 / 4.0)
+                        {
+                            DX.DrawGraph(top.X - 11, top.Y - 12, playerRunHandleR4, DX.TRUE);
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        if (flame <= 1 / 4.0)
+                        {
+                            DX.DrawGraph(top.X - 11, top.Y - 12, playerRunHandleL1, DX.TRUE);
+                            return;
+                        }
+                        if (flame > 1 / 4.0 && flame <= 1 / 2.0)
+                        {
+                            DX.DrawGraph(top.X - 11, top.Y - 12, playerRunHandleL2, DX.TRUE);
+                            return;
+                        }
+                        if (flame > 1 / 2.0 && flame <= 3 / 4.0)
+                        {
+                            DX.DrawGraph(top.X - 11, top.Y - 12, playerRunHandleL3, DX.TRUE);
+                            return;
+                        }
+                        if (flame > 3 / 4.0)
+                        {
+                            DX.DrawGraph(top.X - 11, top.Y - 12, playerRunHandleL4, DX.TRUE);
+                            return;
+                        }
+                    }
+                    break;
+                case MainCharactorStatus.Jump:
+                    if (Direction == Direction.Right)
+                    {
+                        DX.DrawGraph(top.X - 11, top.Y - 12,playerJumpHandleR, DX.TRUE);
+                    }
+                    else
+                    {
+                        DX.DrawGraph(top.X - 11, top.Y - 12, playerJumpHandleL, DX.TRUE);
+                    }
+                    break;
+                case MainCharactorStatus.Fall:
+                    if (Direction == Direction.Right)
+                    {
+                        DX.DrawGraph(top.X - 11, top.Y - 12, playerFallHandleR, DX.TRUE);
+                    }
+                    else
+                    {
+                        DX.DrawGraph(top.X - 11, top.Y - 12, playerFallHandleL, DX.TRUE);
+                    }
+                    break;
+            }
         }
     }
 }
